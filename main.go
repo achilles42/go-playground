8f92b97403c1705d0012bc5adb9fbee7490d9d52
Praveen Shukla <praveen.shukla@go-jek.com>
2018-03-11 13:34:15 +0530

    Add Example: How to handle OS interpts

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
 	"runtime"
+	"syscall"
 	"time"
 )
 
@@ -45,14 +48,14 @@ func NewServer() *Server {
 func (s *Server) run() {
 	for {
 		select {
+		case <-time.After(time.Second):
+			fmt.Println("running task")
 		case <-s.quit:
 			fmt.Println("finishing task")
 			time.Sleep(time.Second)
 			fmt.Println("task done")
 			s.quit <- true
 			return
-		case <-time.After(time.Second):
-			fmt.Println("running task")
 		}
 	}
 }
@@ -64,10 +67,43 @@ func (s *Server) Stop() {
 	fmt.Println("server stopped")
 }
 
+func TestTicker() {
+	ticker := time.NewTicker(500 * time.Millisecond)
+	go func() {
+		for t := range ticker.C {
+			fmt.Println("Tict at ", t)
+		}
+	}()
+	time.Sleep(1600 * time.Millisecond)
+	ticker.Stop()
+	fmt.Println("Ticker stopped")
+}
+
+func OsIntrupt() {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		for {
+			sig := <-sigs
+			fmt.Println()
+			fmt.Println(sig)
+			done <- true
+			return
+		}
+	}()
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
+}
+
 func main() {
-	//testLeakingGoRoutines()
-	//deadLock()
-	s := NewServer()
-	time.Sleep(2 * time.Second)
-	s.Stop()
+	//  testLeakingGoRoutines()
+	//  deadLock()
+	//	s := NewServer()
+	//	time.Sleep(12 * time.Second)
+	//	s.Stop()
+	//	TestTicker()
+	OsIntrupt()
 }
